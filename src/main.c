6bab3e5a03191ebd4eae4b5b14f7df50695adde2
Hukasx0 <hubertkasp13@gmail.com>
2023-03-17 15:38:06 +0100

    Added use of argv

--- src/main.c
+++ src/main.c
@@ -1,8 +1,28 @@
 #include "asminclude.h"
 
-int main(void){
-  const char* data = readFile("src/main.c");
-  writeFile("main_copy.c", data);
-  print("OK\n");
+int main(int argc, char **argv){
+  if (argc != 2){
+    char str1[30] = "Usage: ";
+    char *str2 = argv[0];
+    char *f = str1+7;
+    char str3[] = " <filename>.ty\n";
+    int i = 0;
+    while (*str2){
+      *f = *str2;
+      f++;
+      str2++;
+    }
+    while (str3[i]){
+      *f = str3[i];
+      f++;
+      i++;
+    }
+    *f = '\0';
+    print(str1);
+  }
+  else{
+    const char* fData = readFile(argv[1]);
+    print(fData);
+  }
   return 0;
 }
